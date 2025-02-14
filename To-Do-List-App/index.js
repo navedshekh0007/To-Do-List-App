9f7c29deef568c6d7b05a29f660f3743d085c7c9
naved shekh <143585562+navedshekh0007@users.noreply.github.com>
2025-02-14 22:33:21 +0530

    Add files via upload

---
+++ To-Do-List-App/index.js
@@ -0,0 +1,20 @@
+function addTask() {
+    let newTask = document.createElement('li')
+    let  taskList = document.getElementById('taskList')
+    taskList.appendChild(newTask)
+    newTask.textContent = document.getElementById('inputTask').value
+    document.getElementById('inputTask').value =""
+    deleteTask(newTask)
+
+}
+
+function deleteTask(newTask){
+    const deleteBtn =document.createElement('button')
+    deleteBtn.textContent = "Delete"    
+    newTask.appendChild(deleteBtn)
+    deleteBtn.onclick = function (){
+        newTask.remove()
+    }
+}
+
+
